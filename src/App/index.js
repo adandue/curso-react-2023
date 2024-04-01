41dc6e875054cb771d224bb443e90f45cce81331
adan_due <ing.adanduenas@gmail.com>
2024-04-01 13:38:06 -0600

    Súper poderes para render props y render functions

--- src/App/index.js
+++ src/App/index.js
@@ -44,19 +44,33 @@ function App() {
       error = {error}
       loading = {loading}
       searchedTodos = {searchedTodos}
+      totalTodos = {totalTodos}
+      searchText = {searchValue}
       onError = {() => <TodosError />}
       onLoading = {() => <TodosLoading />}
       onEmptyTodos = {() => <EmptyTodos />}
-      render = {todo => 
+      onEmptySearchResults = {
+        (searchText) => <p>No hay resultados para {searchText}</p>
+        }
+      // render = {todo => 
+      // <TodoItem
+      //   key={todo.text}
+      //   text={todo.text}
+      //   completed={todo.completed}
+      //   onComplete={()=>completeTodo(todo.text)}
+      //   onDelete={()=>deleteTodo(todo.text)}
+      //   />
+      // }
+    >
+    {todo => 
       <TodoItem
-        key={todo.text}
-        text={todo.text}
-        completed={todo.completed}
-        onComplete={()=>completeTodo(todo.text)}
-        onDelete={()=>deleteTodo(todo.text)}
-        />
-      }
-    />
+      key={todo.text}
+      text={todo.text}
+      completed={todo.completed}
+      onComplete={()=>completeTodo(todo.text)}
+      onDelete={()=>deleteTodo(todo.text)}
+      />}
+    </TodoList>
     {!!openModal&&(
       <Modal>
       <TodoForm
