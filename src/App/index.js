721f6b8642de1f839c9b9c8d7e2040e280281efd
adan_due <ing.adanduenas@gmail.com>
2024-04-01 13:04:52 -0600

    Poniendo en oráctica las render props

--- src/App/index.js
+++ src/App/index.js
@@ -39,20 +39,24 @@ function App() {
       setSearchValue={setSearchValue}
     />
     </TodoHeader>
-    <TodoList>
-      {error&& <TodosError/> }
-      {loading&& <TodosLoading/> }
-      {(!loading&&!searchedTodos.length)&&<EmptyTodos/>}
-      {searchedTodos.map(todo=>(
-        <TodoItem
+
+    <TodoList 
+      error = {error}
+      loading = {loading}
+      searchedTodos = {searchedTodos}
+      onError = {() => <TodosError />}
+      onLoading = {() => <TodosLoading />}
+      onEmptyTodos = {() => <EmptyTodos />}
+      render = {todo => 
+      <TodoItem
         key={todo.text}
         text={todo.text}
         completed={todo.completed}
         onComplete={()=>completeTodo(todo.text)}
         onDelete={()=>deleteTodo(todo.text)}
         />
-        ))}
-  </TodoList>
+      }
+    />
     {!!openModal&&(
       <Modal>
       <TodoForm
