3624b0013b214b59b1fe8324c540534b68dbe851
adan_due <ing.adanduenas@gmail.com>
2024-04-02 14:03:22 -0600

    React.Children y React.cloneElement en TodoHeader

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch({searchValue, setSearchValue}) {
+function TodoSearch({searchValue, setSearchValue, loading}) {
 return (
     <input
     placeholder="Utiliza este campo para filtrar tus TODOs"
@@ -10,6 +10,7 @@ return (
     onChange={(event) => {
         setSearchValue(event.target.value);
     }}
+    disabled = {loading}
     />
 );
 }
